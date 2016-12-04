6f15f53c0b2fb6be8ecf3806fbd66e092085f0be
webfarrock <webfarrock@mail.ru>
2016-12-04 16:43:18 +0300

    fin

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,11 +1,15 @@
-export default function({dispatch}){
+export default function ({dispatch}) {
     return next => action => {
-        if(!action.payload || !action.payload.then){
+        
+        console.log('action: ',action);
+        
+        if (!action.payload || !action.payload.then) {
             return next(action);
         }
 
         action.payload.then((response) => {
-           const newAction = {...action, payload: response};
+            const newAction = {...action, payload: response}; 
+            dispatch(newAction);
         });
     };
 
